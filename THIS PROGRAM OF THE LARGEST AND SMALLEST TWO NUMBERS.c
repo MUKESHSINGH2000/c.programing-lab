55e5b3ca476a73474295ee5c6240d3d68f5c8d83
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-05-27 18:23:40 +0530

    Create THIS PROGRAM OF THE LARGEST AND SMALLEST TWO NUMBERS.c

---
+++ THIS PROGRAM OF THE LARGEST AND SMALLEST TWO NUMBERS.c
@@ -0,0 +1,21 @@
+//This programing was developed by Mukesh Singh.
+//Date: 27 may ,2021
+#include <stdio.h>
+
+
+int main() 
+
+{
+    int x,y;
+    printf("THIS PROGRAM OF THE LARGEST AND SMALLEST TWO NUMBERS!\n");
+    printf("\t\tENTER ANY TWO NUMBERS:");
+    scanf("%d %d",&x,&y);
+    if(x>y)
+    
+    printf("\nLARGEST\t\t\t = %d\nSMALLEST\t\t = %d ",x,y);
+    else
+    printf("\nLARGEST\t\t\t = %d\nSMALLEST\t\t = %d ",x,y);
+
+  printf("\n\n\n\t\tTHANKS FOR WATCHING THIS PROGRAM!");
+  return 0;
+}
