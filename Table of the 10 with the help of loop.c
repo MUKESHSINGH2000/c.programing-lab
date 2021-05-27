082a55a0d19c07dd5466945cc8c3ca3205b5ca16
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-05-27 15:41:43 +0530

    Create Table of the 10 with the help of loop.c

---
+++ Table of the 10 with the help of loop.c
@@ -0,0 +1,12 @@
+//This programing was developed by Mukesh Singh.
+//Date: 27 may ,2021
+#include <stdio.h>
+
+int main() {
+    int i;
+  for(int i=10;i<=100; i =i+10)//this loop is a fainate loop.
+  {
+    printf("Hello world......(\t\tThe table of 10:\t\t\=\t\t%d\t\t)\n",i);
+  }
+   return 0;
+}
