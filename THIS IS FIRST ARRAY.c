ddd67a0d00f472d3d7b434b7af4b6b50c94af5ce
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-06-02 11:12:56 +0530

    Create THIS IS FIRST ARRAY.c

---
+++ THIS IS FIRST ARRAY.c
@@ -0,0 +1,31 @@
+//THIS PROGRAM WAS DEVELOPED BY MUKESH SINGH.
+//DATE:2 JUNE ,2021
+#include <stdio.h>
+
+int main()
+{
+    int aa;
+    aa=11;
+printf("Normal variable value:%d\n",aa);   
+ char a[12];
+ a[0]='M';
+ a[1]='U';
+ a[2]='K';
+ a[3]='E';
+ a[4]='S';
+ a[5]='H';
+ a[6]=' ';
+ a[7]='S';
+ a[8]='I';
+ a[9]='N';
+ a[10]='G';
+ a[11]='H';
+
+ printf("Array variable value:\t");
+ for (int i=0; i<12; i++)
+ {
+ printf("%c",a[i]);
+ }
+ printf("\n\n\n THANKS FOR WATCHING THIS PROGRAM!");
+    return 0;
+}
