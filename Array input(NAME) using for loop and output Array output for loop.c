a152d03257ac3c058d5c5c6ac187b3b6a35877e5
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-06-02 11:29:22 +0530

    Create Array input(NAME) using for loop and output Array output for loop.c

---
+++ Array input(NAME) using for loop and output Array output for loop.c
@@ -0,0 +1,25 @@
+//THIS PROGRAM WAS DEVELOPED BY MUKESH SINGH.
+//DATE:2 JUNE ,2021
+#include <stdio.h>
+
+int main()
+{
+
+  
+ char a[12];
+
+
+ printf("Enter the values for Array:");
+ for (int i=0; i<12; i++)
+ {
+ scanf("%c",&a[i]);
+ }
+ printf("Array variable value:\t");
+ for (int i=0; i<12; i++)
+ {
+  printf("%c",a[i]);    
+ }
+ 
+ printf("\n\n\n THANKS FOR WATCHING THIS PROGRAM!");
+    return 0;
+}
