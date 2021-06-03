0f811ed1673813f3b0d8482b3b611e6922fe9cc9
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-06-03 17:50:55 +0530

    Create PROGRAM FOR SIMPLE INTEREST CALCULATION 2.c

---
+++ PROGRAM FOR SIMPLE INTEREST CALCULATION 2.c
@@ -0,0 +1,19 @@
+//THIS PROGRAM WAS DEVELOPED BY MUKESH SINGH.
+//DATE:2 JUNE ,2021
+//PROGRAM FOR SIMPLE INTEREST CALCULATION.
+#include <stdio.h>
+
+int main()
+{
+int amount=4500,year=5;
+float int_rate=4.5,simple_int;
+simple_int=(amount*year*int_rate)/100;
+printf("\n Amount%d Rs.",amount);
+
+printf("\n Rate of Interest=%d",int_rate);
+printf("\n Duration = %d years",year);
+printf("\n The simple interest calculated is = Rs.%f",simple_int);
+     printf("\n\n\n THANKS FOR WATCHING THIS PROGRAM!");
+    
+    return 0;
+}
