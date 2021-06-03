633f2ec82842283d4754a77a15db3e8475cbb839
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-06-03 17:45:58 +0530

    Create SIMPLE INTEREST PROGRAM.c

---
+++ SIMPLE INTEREST PROGRAM.c
@@ -0,0 +1,27 @@
+//THIS PROGRAM WAS DEVELOPED BY MUKESH SINGH.
+//DATE:2 JUNE ,2021
+//PROGRAM FOR SIMPLE INTEREST CALCULATION.
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+float c,d;
+int a,b;
+char x;
+a=1,2,3,4;
+c=16.4358;
+x='A';
+printf("\nEnter a number:");
+scanf("%2d",&b);
+printf("\nEnter a floating point number:");
+scanf("%f",&d);
+printf("\nA=%2d \nB%2d",a,b);
+printf("\nC=%f \nD%f",c,d);
+printf("\nC=%2f \nD=%2f",c,d);
+printf("\n=%c \n%3c \n%5c",x,x,x);
+printf("\n=%3c \n%c",x,x);
+     printf("\n\n\n THANKS FOR WATCHING THIS PROGRAM!");
+    
+    return 0;
+}
