2135b10aceedd1c5a014cd28b37cc70f835837e6
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-06-03 17:52:41 +0530

    Create SIMPLE ARRAY PROGRAM.c

---
+++ SIMPLE ARRAY PROGRAM.c
@@ -0,0 +1,20 @@
+//THIS PROGRAM WAS DEVELOPED BY MUKESH SINGH.
+//DATE:2 JUNE ,2021
+#include <stdio.h>
+
+int main()
+{
+    int aa;
+    aa=10;
+printf("Normal variable value:%d\n",aa);   
+    int a[5];
+    a[0]=11;
+    a[1]=12;
+    a[2]=13;
+    a[3]=14;
+    a[4]=15;
+    printf("Array variabe at a[0]:%d\n",a[0]);
+ 
+    
+    return 0;
+}
