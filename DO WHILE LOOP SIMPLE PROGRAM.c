4a79d8b4320ac10fb98d0039f900f4824b0a8e18
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-06-03 17:44:01 +0530

    Create DO WHILE LOOP SIMPLE PROGRAM.c

---
+++ DO WHILE LOOP SIMPLE PROGRAM.c
@@ -0,0 +1,23 @@
+// THIS PROGRAM WAS DEVELOPED BY MUKESH SINGH.
+//DATE:3JUNE,2021.
+//DO WHILE LOOP.
+#include <stdio.h>
+
+int main() 
+{
+    int i=0;
+    while(i<5)
+    {
+    printf("Hello  poornima of valuei:%d\n",i);
+    i++;
+    }
+        printf("..................\n");
+    i=0;
+    do
+    {
+        printf("hello poorinima value of i:%d\n",i);
+        i++;
+     }   while(i<5);
+    
+    return 0;
+}
