b3c5d1752de79bb28d6c54d15c3856373655d0e5
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-07-23 10:22:53 +0530

    Create Write a program to use Structure to store information (Roll_no, Age, and Section) of 5 Students.c

---
+++ Write a program to use Structure to store information (Roll_no, Age, and Section) of 5 Students.c
@@ -0,0 +1,25 @@
+//THIS PROGRAM WAS DEVELOPED BY MUKESH.
+//DATE:23JULY,2021
+#include <stdio.h>
+struct Student
+{
+    int roll_no;
+    int age;
+} stu1, stu2, stu3;
+
+int main()
+{
+    int sum;
+    printf("Please enter Roll_no and Age of students\n");
+    scanf("%d", &stu1.roll_no);
+    scanf("%d", &stu1.age);
+    
+    scanf("%d", &stu2.roll_no);
+    scanf("%d", &stu2.age);
+    
+    scanf("%d", &stu3.roll_no);
+    scanf("%d", &stu3.age);
+    
+    sum=stu1.age+stu2.age+stu3.age;
+    printf("What is the average age in Class: %d", sum/3);
+    return 0;
