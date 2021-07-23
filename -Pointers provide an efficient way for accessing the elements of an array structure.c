b1bf4b09c0fbd4c27541fcb60caa9c9a85595b88
MUKESHSINGH2000 <83683355+MUKESHSINGH2000@users.noreply.github.com>
2021-07-23 10:40:58 +0530

    Create -Pointers provide an efficient way for accessing the elements of an array structure.c

---
+++ -Pointers provide an efficient way for accessing the elements of an array structure.c
@@ -0,0 +1,24 @@
+//THIS PROGRAM WAS DEVELOPED BY MUKESH.
+//DATE:23JULY,2021
+#include <stdio.h>
+int main()
+{
+   int *pc, **qc, c;
+    c = 22;
+    pc = &c;
+    qc = &pc;
+   printf("Address of c: %p\n", &c);
+   printf("Address stored at pointer pc: %p\n", pc);
+   
+   printf("Address of pointer pc: %p\n", &pc);
+   printf("Address stored at pointer qc: %p\n", qc);
+   
+   printf("Value stored at c:%d\n", c);
+   printf("Value stored at c using pc:%d\n", *pc);
+   printf("Value stored at c using qc:%d\n", **qc);
+   **qc=14;
+   printf("Value stored at c:%d\n", c);
+   printf("Value stored at c using pc:%d\n", *pc);
+   printf("Value stored at c using qc:%d\n", **qc);
+   return 0;
+}
